0c18a7e7d69252512c4bd7d0423a59c567c3a69e
Owez <root@ogriffiths.com>
2021-09-11 18:15:17 +0100

    Tested release.sh, closes #2

--- release.sh
+++ release.sh
@@ -1,8 +1,23 @@
+# start msg
 echo "Starting release process.."
+
+# build binary
 cargo build --release
 mkdir ./holenews-release/
 mv ./target/release/holenews ./holenews-release/
 strip ./holenews-release/holenews
+
+# add content
 cp -r ./templates/ ./holenews-release/
 cp -r ./static/ ./holenews-release/
+cp -r ./migrations/ ./holenews-release/
+
+# database
+cd ./holenews-release/
+sqlx database create
+sqlx migrate run
+rm -rf ./migrations/
+cd ..
+
+# finish msg
 echo "Finished release, see the holenews-release/ directory"
